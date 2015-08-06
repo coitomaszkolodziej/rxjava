e6ca8e2ac25dcf5b9e69ee639d16c98e7d06b09f
= <markon012@gmail.com>
2015-08-06 09:10:22 +0200

    aktualizacja pliku main

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import random
 import psycopg2
 import sys
 
+namesPath='C:/Users/Praktyki/Desktop/InputData/names.txt'
+surnamesPath='C:/Users/Praktyki/Desktop/InputData/surnames.txt'
+countriesPath='C:/Users/Praktyki/Desktop/InputData/countries.txt'
+cityListPath='C:/Users/Praktyki/Desktop/InputData/city-list.txt'
+
 def makeConnection():
     connString = "host = 'localhost' dbname = 'artistsandsongs' user = 'admin'  password = 'admin'"
     conn = psycopg2.connect(connString)
@@ -25,54 +30,52 @@ def clearAlbums():
     conn.commit()
     conn.close()
 
-def generateArtists(n):
+def clearSongs():
+    conn = makeConnection()
+    cursor = conn.cursor()
+    cursor.execute("ALTER SEQUENCE songs_song_id_seq RESTART WITH 1")
+    cursor.execute("TRUNCATE songs CASCADE")
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+def generate(n,names,surnames,countries,cityList):
     conn_string = "host='localhost' dbname='artistsandsongs' user='admin' password='admin'"
     print "Connecting to database\n	->%s" % (conn_string)
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
 
-
-    print "Connected to Artists!\n"
-    with open('C:/Users/Praktyki/Desktop/bezzab/names.txt') as f:
+    clearArtists()
+    clearAlbums()
+    clearSongs()
+    with open(names) as f:
         listOfNames = f.read().splitlines()
-    with open('C:/Users/Praktyki/Desktop/bezzab/surnames.txt') as f:
+    with open(surnames) as f:
         listOfSurnames = f.read().splitlines()
     for i in range(1, n+1):
         cursor.execute("INSERT INTO artists( artist_name, artist_surname, artist_age) VALUES(%s, %s, %s)", (unicode(random.choice(listOfNames), "utf-8"),
                                                                                                         unicode(random.choice(listOfSurnames), "utf-8"),
-                                                                                              unicode(str(random.randrange(20,80)), "utf-8")))
-    cursor.close()
-    conn.commit()
-    conn.close()
+                                                                                                        unicode(str(random.randrange(20,80)), "utf-8")))
 
+        k=random.randrange(1,5)
+        with open(countries) as f:
+            listOfCountries = f.read().splitlines()
+            for j in range(1, k+1):
+                cursor.execute("INSERT INTO albums (album_name,artist_id) VALUES(%s,%s)", (unicode(random.choice(listOfCountries), "utf-8"),
+                                                                                           i))
+                cursor.execute("SELECT album_id from albums ORDER BY album_id DESC LIMIT 1 ")
+                numberOfAlbums = cursor.fetchone()[0]
 
-def generateAlbums(n):
-    conn_string = "host='localhost' dbname='artistsandsongs' user='admin' password='admin'"
-    print "Connecting to database\n	->%s" % (conn_string)
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-
-    print "Connected to Albums!\n"
-    with open('C:/Users/Praktyki/Desktop/bezzab/countries.txt') as f:
-        listOfCountries = f.read().splitlines()
-    cursor.execute("SELECT COUNT(artist_name) FROM ARTISTS")
-    k=cursor.fetchall()
-    for i in range(1, n+1):
-        cursor.execute("INSERT INTO albums (album_name,artist_id) VALUES(%s,%s)", (unicode(random.choice(listOfCountries), "utf-8"),
-                                                                                   unicode(str(random.randrange(1,k[0][0])), "utf-8")))
+                o=random.randrange(1,10)
+                with open(cityList) as f:
+                    listOfCities = f.read().splitlines()
+                    for w in range(1, o+1):
+                        cursor.execute("INSERT INTO songs (song_name,album_id) VALUES(%s,%s)", (unicode(random.choice(listOfCities), "utf-8"),
+                                                                                            numberOfAlbums))
 
     cursor.close()
     conn.commit()
     conn.close()
 
 
-
-def generateData(n,m,k):
-    clearArtists()
-    generateArtists(n)
-    clearAlbums()
-    generateAlbums(m)
-    clearSongs()
-    generateSongs(k)
-
-wywolajFunkcje(100,200,400)
+generate(1000,namesPath,surnamesPath,countriesPath,cityListPath)
